0f3855a5aaf34e8b1d962a2c0580735bc7318fb1
DONDONINI-PC\Edisson Flores <flores96@gmail.com>
2017-03-22 16:05:33 -0400

    AGP-Assignment3 Version 0.0.2.0 Unity Version: 5.5.2f1
    - Started implementing game logic
    - Created 3D models for player and AI markers
    - Added EasingFunction script
    - Added ShowOnlyAttribute inspector script

---
+++ Assets/Scripts/Game/Board.cs
@@ -0,0 +1,109 @@
+﻿public struct Slots
+{
+    private int InternalValue { get; set; }
+
+    public static readonly int Blank = 0;
+    public static readonly int AI = -1;
+    public static readonly int Player = 1;
+
+    public override bool Equals(object obj)
+    {
+        Slots otherObj = (Slots)obj;
+        return otherObj.InternalValue.Equals(InternalValue);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public static bool operator ==(Slots val, Slots other)
+    {
+        return (val.InternalValue == other.InternalValue);
+    }
+
+    public static bool operator ==(Slots val, int other)
+    {
+        return (val.InternalValue == other);
+    }
+
+    public static bool operator !=(Slots val, Slots other)
+    {
+        return (val.InternalValue != other.InternalValue);
+    }
+
+
+    public static bool operator !=(Slots val, int other)
+    {
+        return (val.InternalValue != other);
+    }
+
+    public static int operator *(Slots val, int other)
+    {
+        return (val.InternalValue * other);
+    }
+
+    public static int operator *(Slots val, Slots other)
+    {
+        return (val.InternalValue * other.InternalValue);
+    }
+
+    public static implicit operator Slots(int otherType)
+    {
+        return new Slots
+        {
+            InternalValue = otherType
+        };
+    }
+
+    public int ToInt()
+    {
+        return InternalValue;
+    }
+}
+
+public class Board : GameMaster {
+
+    // Board
+    private Slots[] board;
+
+    // Constructor
+    public Board()
+    {
+        board = new Slots[9];
+    }
+
+    /// <summary>
+    /// Get the board
+    /// </summary>
+    /// <returns>Board instance</returns>
+    public Slots[] GetBoard()
+    {
+        return board;
+    }
+
+    public Slots GetBoardSlot(int pos)
+    {
+        return board[pos];
+    }
+
+    /// <summary>
+    /// Replace entire board with new board
+    /// </summary>
+    /// <param name="newBoard">New board</param>
+    public void SetBoard(Slots[] newBoard)
+    {
+        board = newBoard;
+    }
+
+    /// <summary>
+    /// Set slot type
+    /// </summary>
+    /// <param name="slotType">Slot type</param>
+    /// <param name="xPos">X Position</param>
+    /// <param name="yPos">Y Position</param>
+    public void SetBoardSlot(Slots slotType, int pos)
+    {
+        board[pos] = slotType;
+    }
+}
---
+++ Assets/Scripts/Game/GameMaster.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MarkerType
+{
+    X = 0,
+    O = 1
+}
+
+public class GameMaster : MonoBehaviour {
+
+    [SerializeField][ShowOnly]
+    private Board m_board;
+
+    [SerializeField][ShowOnly]
+    private int m_turns = 0;
+
+    public int m_totalTurns = 9;
+
+    public GameObject m_oMarker;
+
+    public GameObject m_xMarker;
+
+    public MarkerType playerMarker;
+    public MarkerType AIMarker;
+
+    [SerializeField]
+    private Transform[] slotPositions;
+
+    [SerializeField]
+    private GameObject[] slotButtons;
+
+    private GameObject[] m_markers;
+
+	// Use this for initialization
+	void Start () {
+        m_board = new Board();
+        m_markers = new GameObject[9];
+
+        ComputerMove(m_board);
+
+        playerMarker = MarkerType.O;
+        AIMarker = MarkerType.O;
+
+        UpdateBoardVisuals();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    Slots Win(Board currentBoard) 
+    {
+        Slots[] board = currentBoard.GetBoard();
+
+        // Determines if a player has won, returns 0 otherwise.
+        int[,] wins = new int[8,3]{ { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 2, 4, 6 } };
+	    for (int i = 0; i< 8; ++i)
+        {
+
+            if (
+                board[wins[i, 0]] != Slots.Blank &&
+                board[wins[i, 0]] == board[wins[i, 1]] &&
+                board[wins[i, 0]] == board[wins[i, 2]]
+            )
+            {
+                return currentBoard.GetBoardSlot(wins[i, 2]);
+            }
+	    }
+
+	    return Slots.Blank;
+    }
+
+    int MinMax(Board currentBoard, Slots player)
+    {
+        Slots[] board = currentBoard.GetBoard();
+
+        //How is the position like for player (their turn) on board?
+        Slots winner = Win(currentBoard);
+        if (winner != 0) return winner * player;
+
+        int move = -1;
+        int score = -2;//Losing moves are preferred to no move
+        int i;
+        for (i = 0; i < 9; ++i)
+        {
+            //For all moves,
+            if (board[i] == Slots.Blank)
+            {
+                //If legal,
+                board[i] = player;//Try the move
+
+                int thisScore = -MinMax(currentBoard, player * -1);
+                if (thisScore > score)
+                {
+                    score = thisScore;
+                    move = i;
+                }//Pick the one that's worst for the opponent
+
+                board[i] = 0;//Reset board after try
+            }
+        }
+        if (move == -1) return 0;
+        return score;
+    }
+
+    void ComputerMove(Board currentBoard)
+    {
+        Slots[] board = currentBoard.GetBoard();
+
+        int move = -1;
+        int score = -2;
+        int i;
+        for (i = 0; i < 9; ++i)
+        {
+            if (board[i] == 0)
+            {
+                board[i] = 1;
+                int tempScore = -MinMax(currentBoard, Slots.AI);
+                board[i] = 0;
+                if (tempScore > score)
+                {
+                    score = tempScore;
+                    move = i;
+                }
+            }
+        }
+        //returns a score based on minimax tree at a given node.
+        board[move] = 1;
+    }
+
+    void UpdateBoardVisuals()
+    {
+        Slots[] board = m_board.GetBoard();
+
+
+
+        for (int i = 0; i < m_markers.Length; i++)
+        {
+            switch(board[i].ToInt())
+            {
+                case -1:
+                    PlaceMarker(playerMarker, i);
+                    //GameObject newMarker = Instantiate()
+                    break;
+                case 1:
+                    PlaceMarker(AIMarker, i);
+                    break;
+                default:
+                    if (m_markers[i])
+                    {
+                        Destroy(m_markers[i]);
+                        m_markers[i] = null;
+                    }
+                    break;
+            }
+        }
+    }
+
+    bool PlaceMarker(MarkerType selectedMarker, int pos)
+    {
+        // If marker already exists in place. Return as fail
+        if (m_markers[pos])
+        {
+            return false;
+        }
+
+        GameObject newMarker;
+
+        switch (selectedMarker)
+        {
+            case MarkerType.O:
+                newMarker = Instantiate(m_oMarker) as GameObject;
+                break;
+            case MarkerType.X:
+                newMarker = Instantiate(m_xMarker) as GameObject;
+                break;
+            default:
+                return false;
+        }
+
+        // New marker needs it be placed in the right spot if exists
+        if (newMarker)
+        {
+            newMarker.transform.position = slotPositions[pos].position;
+            newMarker.transform.SetParent(slotPositions[pos]);
+        }
+
+        return true; // Success
+    }
+}
